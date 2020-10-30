e82d7b6af24f045d545e7def985e7a1fba2aca91
brook <lilisong9920@gmail.com>
2020-10-30 15:51:20 +0800

    -- 订单新增

--- renren-fast-vue-master/src/views/modules/order/order-add-or-update.vue
+++ renren-fast-vue-master/src/views/modules/order/order-add-or-update.vue
@@ -2,31 +2,154 @@
   <el-dialog
     :title="!dataForm.id ? '新增' : '修改'"
     :close-on-click-modal="false"
-    :visible.sync="visible">
-    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="80px">
-      <el-form-item label="酒店" prop="hotelId" :class="{ 'is-required': !dataForm.id }">
-        <el-select v-model="dataForm.hotelId" @change="$forceUpdate()" filterable clearable placeholder="请选择"
-                   style="width: 100%; position: relative" :disabled="!!dataForm.id">
-          <el-option v-for="hotel in hotels" :key="hotel.id" :label="hotel.name" :value="hotel.id">
+    :visible.sync="visible"
+  >
+    <el-form
+      :model="dataForm"
+      :rules="dataRule"
+      ref="dataForm"
+      @keyup.enter.native="dataFormSubmit()"
+      label-width="80px"
+    >
+      <el-form-item
+        label="酒店"
+        prop="hotelId"
+        :class="{ 'is-required': !dataForm.id }"
+      >
+        <el-select
+          v-model="dataForm.hotelId"
+          @change="$forceUpdate()"
+          filterable
+          clearable
+          placeholder="请选择"
+          style="width: 100%; position: relative"
+          :disabled="!!dataForm.id"
+        >
+          <el-option
+            v-for="hotel in hotels"
+            :key="hotel.id"
+            :label="hotel.name"
+            :value="hotel.id"
+          >
           </el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="安装公司" prop="installationId" :class="{ 'is-required': !dataForm.id }">
-        <el-select v-model="dataForm.installationId" @change="$forceUpdate()" filterable clearable placeholder="请选择"
-                   style="width: 100%; position: relative" :disabled="!!dataForm.id">
-          <el-option v-for="installation in installations" :key="installation.id" :label="installation.name" :value="installation.id">
+      <el-form-item
+        label="安装公司"
+        prop="installationId"
+        :class="{ 'is-required': !dataForm.id }"
+      >
+        <el-select
+          v-model="dataForm.installationId"
+          @change="$forceUpdate()"
+          filterable
+          clearable
+          placeholder="请选择"
+          style="width: 100%; position: relative"
+          :disabled="!!dataForm.id"
+        >
+          <el-option
+            v-for="installation in installations"
+            :key="installation.id"
+            :label="installation.name"
+            :value="installation.id"
+          >
           </el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="支付方式" prop="payType" :class="{ 'is-required': !dataForm.id }">
-        <el-select v-model="dataForm.payType" @change="$forceUpdate()" filterable clearable placeholder="请选择"
-                   style="width: 100%; position: relative" :disabled="!!dataForm.id">
-          <el-option v-for="payType in payTypes" :key="payType.value" :label="payType.label" :value="payType.value">
+
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-form-item
+            label="设备类型"
+            prop="deviceType"
+            :class="{ 'is-required': !dataForm.id }"
+          >
+            <el-select
+              v-model="dataForm.deviceType"
+              @change="deviceTypeChange()"
+              placeholder="请选择"
+              style="width: 100%; position: relative"
+              :disabled="!!dataForm.id"
+            >
+              <el-option
+                v-for="type in deviceType"
+                :key="type.id"
+                :label="type.name"
+                :value="type.id"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+
+        <el-col :span="16">
+          <el-form-item
+            label="设备名称"
+            prop="deviceName"
+            :class="{ 'is-required': !dataForm.id }"
+          >
+            <el-select
+              v-model="dataForm.deviceName"
+              @change="deviceChange"
+              filterable
+              clearable
+              placeholder="请选择"
+              style="width: 100%; position: relative"
+              :disabled="!!dataForm.id"
+            >
+              <el-option
+                v-for="d in deviceList"
+                :key="d.id"
+                :label="d.name"
+                :value="d.id"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-form-item
+        label="设备数量"
+        prop="deviceNum"
+        :class="{ 'is-required': !dataForm.id }"
+      >
+        <el-input-number
+          v-model="dataForm.deviceNum"
+          :min="1"
+          :max="max"
+          placeholder="请填写设备数量"
+        ></el-input-number>
+      </el-form-item>
+
+      <el-form-item
+        label="支付方式"
+        prop="payType"
+        :class="{ 'is-required': !dataForm.id }"
+      >
+        <el-select
+          v-model="dataForm.payType"
+          @change="$forceUpdate()"
+          filterable
+          clearable
+          placeholder="请选择"
+          style="width: 100%; position: relative"
+          :disabled="!!dataForm.id"
+        >
+          <el-option
+            v-for="payType in payTypes"
+            :key="payType.value"
+            :label="payType.label"
+            :value="payType.value"
+          >
           </el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="优惠券" prop="couponSn">
-        <el-input v-model="dataForm.couponSn" placeholder="优惠券序列号"></el-input>
+        <el-input
+          v-model="dataForm.couponSn"
+          placeholder="优惠券序列号"
+        ></el-input>
       </el-form-item>
       <el-form-item label="支付金额" prop="amount">
         <el-input v-model="dataForm.amount" :disabled="true"></el-input>
@@ -46,127 +169,200 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        visible: false,
-        hotels: [],
-        installations: [],
-        payTypes: [{
-          label: '支付宝', value: 1
-        }, {
-          label: '微信', value: 2
-        }, {
-          label: '银行卡', value: 3
-        }],
-        dataForm: {
-          id: 0,
-          hotelId: '',
-          installationId: '',
-          payType: '',
-          couponSn: '',
-          amount: '',
-          realAmount: '',
-          remark: ''
+export default {
+  data() {
+    return {
+      visible: false,
+      hotels: [],
+      installations: [],
+
+      max: 10,
+
+      payTypes: [
+        {
+          label: "支付宝",
+          value: 1,
         },
-        dataRule: {
-          hotelId: [
-            { required: true, message: '酒店不能为空', trigger: 'blur' }
-          ],
-          installationId: [
-            { required: true, message: '安装公司不能为空', trigger: 'blur' }
-          ],
-          payType: [
-            { required: true, message: '支付方式不能为空', trigger: 'blur' }
-          ]
+        {
+          label: "微信",
+          value: 2,
+        },
+        {
+          label: "银行卡",
+          value: 3,
+        },
+      ],
+      dataForm: {
+        id: 0,
+        hotelId: "",
+        installationId: "",
+        payType: "",
+        couponSn: "",
+        amount: "",
+        realAmount: "",
+        remark: "",
+        deviceType: "", // 0主机1设备
+        deviceName: "", // 设备名称ID
+      },
+      dataRule: {
+        hotelId: [{ required: true, message: "酒店不能为空", trigger: "blur" }],
+        installationId: [
+          { required: true, message: "安装公司不能为空", trigger: "blur" },
+        ],
+        payType: [
+          { required: true, message: "支付方式不能为空", trigger: "blur" },
+        ],
+        deviceType: [
+          { required: true, message: "设备类型不能为空", trigger: "blur" },
+        ],
+        deviceName: [
+          { required: true, message: "设备名称不能为空", trigger: "blur" },
+        ],
+        deviceNum: [
+          { required: true, message: "设备数量不能为空", trigger: "blur" },
+        ],
+      },
+      deviceType: [], // 设备类型select
+      deviceList: [], // 设备名称select
+      device: [], // 智能设备
+      equip: [], // 智能主机
+    };
+  },
+  methods: {
+    init(id) {
+      this.dataForm.id = id || 0;
+      this.visible = true;
+      this.$nextTick(() => {
+        this.$refs["dataForm"].resetFields();
+        this.$http({
+          url: this.$http.adornUrl("/org/select"),
+          method: "get",
+          params: this.$http.adornParams({
+            userType: 2,
+          }),
+        }).then(({ data }) => {
+          this.hotels = data && data.code === 0 ? data.data : [];
+        });
+        this.$http({
+          url: this.$http.adornUrl("/org/select"),
+          method: "get",
+          params: this.$http.adornParams({
+            userType: 3,
+          }),
+        }).then(({ data }) => {
+          this.installations = data && data.code === 0 ? data.data : [];
+        });
+
+        // 获取设备类型&列表
+        this.$http({
+          url: this.$http.adornUrl("/order/order/equipList"),
+          method: "get",
+          params: this.$http.adornParams(),
+        }).then(({ data }) => {
+          this.equip = data.equip || [];
+          this.device = data.device || [];
+
+          let type = [];
+          if (data.equip && data.equip.length > 0) {
+            type.push({
+              id: 0,
+              name: "智能主机",
+            });
+          }
+          if (data.device && data.device.length > 0) {
+            type.push({
+              id: 1,
+              name: "智能设备",
+            });
+          }
+          this.deviceType = type;
+        });
+
+        if (this.dataForm.id) {
+          this.$http({
+            url: this.$http.adornUrl(`/order/order/info/${this.dataForm.id}`),
+            method: "get",
+            params: this.$http.adornParams(),
+          }).then(({ data }) => {
+            if (data && data.code === 0) {
+              this.dataForm.hotelId = data.order.hotelId;
+              this.dataForm.hotelName = data.order.hotelName;
+              this.dataForm.installationId = data.order.installationId;
+              this.dataForm.installationName = data.order.installationName;
+              this.dataForm.payType = data.order.payType;
+              this.dataForm.couponSn = data.order.couponSn;
+              this.dataForm.couponId = data.order.couponId;
+              this.dataForm.remark = data.order.remark;
+            }
+          });
         }
+      });
+    },
+    // 表单提交
+    dataFormSubmit() {
+      this.$refs["dataForm"].validate((valid) => {
+        if (valid) {
+          const hotel = this.hotels.find((item) => {
+            return item.id === this.dataForm.hotelId;
+          });
+          const installation = this.installations.find((item) => {
+            return item.id === this.dataForm.installationId;
+          });
+          this.$http({
+            url: this.$http.adornUrl(
+              `/order/order/${!this.dataForm.id ? "save" : "update"}`
+            ),
+            method: "post",
+            data: this.$http.adornData({
+              id: this.dataForm.id || undefined,
+              hotelId: this.dataForm.hotelId,
+              hotelName: hotel.name,
+              installationId: this.dataForm.installationId,
+              installationName: installation.name,
+              payType: this.dataForm.payType,
+              couponSn: this.dataForm.couponSn,
+              remark: this.dataForm.remark,
+            }),
+          }).then(({ data }) => {
+            if (data && data.code === 0) {
+              this.$message({
+                message: "操作成功",
+                type: "success",
+                duration: 1500,
+                onClose: () => {
+                  this.visible = false;
+                  this.$emit("refreshDataList");
+                },
+              });
+            } else {
+              this.$message.error(data.msg);
+            }
+          });
+        }
+      });
+    },
+    deviceTypeChange() {
+      this.$forceUpdate();
+      this.dataForm.deviceName = "";
+      if (this.dataForm.deviceType == 0) {
+        // 智能主机
+        this.deviceList = this.equip;
+      } else {
+        // 智能设备
+        this.deviceList = this.device;
       }
     },
-    methods: {
-      init (id) {
-        this.dataForm.id = id || 0
-        this.visible = true
-        this.$nextTick(() => {
-          this.$refs['dataForm'].resetFields()
-          this.$http({
-            url: this.$http.adornUrl('/org/select'),
-            method: 'get',
-            params: this.$http.adornParams({
-              userType: 2
-            })
-          }).then(({data}) => {
-            this.hotels = data && data.code === 0 ? data.data : []
-          })
-          this.$http({
-            url: this.$http.adornUrl('/org/select'),
-            method: 'get',
-            params: this.$http.adornParams({
-              userType: 3
-            })
-          }).then(({data}) => {
-            this.installations = data && data.code === 0 ? data.data : []
-          })
-          if (this.dataForm.id) {
-            this.$http({
-              url: this.$http.adornUrl(`/order/order/info/${this.dataForm.id}`),
-              method: 'get',
-              params: this.$http.adornParams()
-            }).then(({data}) => {
-              if (data && data.code === 0) {
-                this.dataForm.hotelId = data.order.hotelId
-                this.dataForm.hotelName = data.order.hotelName
-                this.dataForm.installationId = data.order.installationId
-                this.dataForm.installationName = data.order.installationName
-                this.dataForm.payType = data.order.payType
-                this.dataForm.couponSn = data.order.couponSn
-                this.dataForm.couponId = data.order.couponId
-                this.dataForm.remark = data.order.remark
-              }
-            })
-          }
-        })
-      },
-      // 表单提交
-      dataFormSubmit () {
-        this.$refs['dataForm'].validate((valid) => {
-          if (valid) {
-            const hotel = this.hotels.find(item => {
-              return item.id === this.dataForm.hotelId
-            })
-            const installation = this.installations.find(item => {
-              return item.id === this.dataForm.installationId
-            })
-            this.$http({
-              url: this.$http.adornUrl(`/order/order/${!this.dataForm.id ? 'save' : 'update'}`),
-              method: 'post',
-              data: this.$http.adornData({
-                'id': this.dataForm.id || undefined,
-                'hotelId': this.dataForm.hotelId,
-                'hotelName': hotel.name,
-                'installationId': this.dataForm.installationId,
-                'installationName': installation.name,
-                'payType': this.dataForm.payType,
-                'couponSn': this.dataForm.couponSn,
-                'remark': this.dataForm.remark
-              })
-            }).then(({data}) => {
-              if (data && data.code === 0) {
-                this.$message({
-                  message: '操作成功',
-                  type: 'success',
-                  duration: 1500,
-                  onClose: () => {
-                    this.visible = false
-                    this.$emit('refreshDataList')
-                  }
-                })
-              } else {
-                this.$message.error(data.msg)
-              }
-            })
-          }
-        })
+    deviceChange() {
+      this.$forceUpdate();
+      if (this.dataForm.deviceType == 0) {
+        this.max = 1;
+      } else {
+        const device = this.deviceList.find((item) => {
+          return item.id === this.dataForm.deviceName;
+        });
+        this.max = device.amount;
       }
-    }
-  }
+    },
+  },
+};
 </script>
